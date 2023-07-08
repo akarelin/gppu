502ca50acccf08a048379e3501b1eea14a2a478e
Alex Karelin <alex@karelin.com>
2023-07-07 18:35:55 -0700

    Insteon Indicators service

--- gppu.py
+++ gppu.py
@@ -124,7 +124,22 @@ def dict_from_yml(filename:str):
 
 """Logging"""
 def timestamp(): return datetime.now().strftime("%Y%m%d.%H%M%S")
-
+def pretty_timedelta(ts):
+  now = datetime.now().timestamp()
+  delta = now - ts
+  seconds = int(delta)
+  days, seconds = divmod(seconds, 86400)
+  hours, seconds = divmod(seconds, 3600)
+  minutes, seconds = divmod(seconds, 60)
+  if days > 0:
+    return '%dd %dh %dm %ds' % (days, hours, minutes, seconds)
+  elif hours > 0:
+    return '%dh %dm %ds' % (hours, minutes, seconds)
+  elif minutes > 0:
+    return '%dm %ds' % (minutes, seconds)
+  else:
+    return '%ds' % (seconds,)
+    
 def pfy(object) -> str: return "\n"+pprint.pformat(object, indent=4, width=40, compact=True)
 
 def _print_terminal_color_table():
